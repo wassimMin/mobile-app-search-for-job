e8b03e7b5f545b91408616215d9e1e57a7330a8d
Younsi wassim <120186938+wassimMin@users.noreply.github.com>
2024-06-08 22:19:57 +0100

    ww

--- gulp/app/build.gradle.kts
+++ gulp/app/build.gradle.kts
@@ -58,4 +58,12 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("com.google.android.material:material:1.4.0")
 }
